b4635b4a2fadabafd8acb66b75734e13f3bc67d0
eltonina <eltonina.hotmail.com>
2019-06-30 20:08:38 +0200

    Implement a call to Api fill a Table

--- src/index.js
+++ src/index.js
@@ -1,6 +1,28 @@
-import numeral from 'numeral';
-import './index.css';
+import {getUsers} from './api/userApi';
 
-const courseValue = numeral(1000).format('0.0,00€');
-// eslint-disable-next-line no-console
-console.log(`Have to pay ${courseValue} blabla!`);
+// Populating the table via API call
+getUsers().then(result => {
+  let usersBody = "";
+
+  // eslint-disable-next-line no-debugger
+  debugger;
+  result.forEach(user => {
+    usersBody += `<tr>
+      <td><a href="#" data-id="${user.id}" class="deleteUser">Delete</a></td>
+      <td>${user.id}</td>
+      <td>${user.firstName}</td>
+      <td>${user.lastName}</td>
+      <td>${user.email}</td>
+    </tr>
+    `
+    global.document.getElementById('users').innerHTML = usersBody;
+  });
+})
+
+
+// import numeral from 'numeral';
+// import './index.css';
+
+// const courseValue = numeral(1000).format('0.0,00€');
+// // eslint-disable-next-line no-console
+// console.log(`Have to pay ${courseValue} blabla!`);
